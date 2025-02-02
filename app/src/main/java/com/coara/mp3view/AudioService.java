47aa931a0fe983c8572f4b8a09ec14a7d34ea8d1
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-02-02 12:34:25 +0900

    Update AudioService.java

--- app/src/main/java/com/coara/mp3view/AudioService.java
+++ app/src/main/java/com/coara/mp3view/AudioService.java
@@ -110,13 +110,13 @@ public class AudioService extends Service {
         int iconRes;
         switch (playbackStatus) {
             case "PLAY":
-                iconRes = android.R.drawable.ic_media_play;
+                iconRes = R.drawable.ic_playing;
                 break;
             case "PAUSE":
-                iconRes = android.R.drawable.ic_media_pause;
+                iconRes = R.drawable.ic_paused;
                 break;
             default:
-                iconRes = android.R.drawable.ic_media_stop;
+                iconRes = R.drawable.ic_stopped;
         }
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
