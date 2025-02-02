9cf4c48169794f39a9a891f8b8527080d4185b7a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-02-02 13:44:25 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/mp3view/MainActivity.java
+++ app/src/main/java/com/coara/mp3view/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.net.Uri;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
